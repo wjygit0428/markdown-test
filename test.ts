89fb101aff11730d9329345e3b34978ef6b3d51e
曾曼青 <2192073703@qq.com>
2024-10-09 17:52:39 +0800

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,6 @@
+export const ERROR_color_2 = '#cf4b6e';
+export const WARNING_COLOR_2 = '#f1c062';
+      export const INFO_COLOR_2 = '#5baae7';
+export const GRAY_COLOR_2 = '#eeeeee';
+export const BRAND_COLOR_5 = '#415B94'
+export const GRAY_COLOR_5 = '#2f2f31';
