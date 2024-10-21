7e32647d0dae560102ab929ef33f820fec5311e1
曾曼青 <2192073703@qq.com>
2024-10-21 14:25:01 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -18,3 +18,23 @@ if (!key in object) {
     // operator precedence makes it equivalent to (!key) in object
     // and type conversion makes it equivalent to (key ? "false" : "true") in object
 }
+
+/* eslint-disable no-alert, no-console */
+
+function badFunction() {
+  alert('This is an alert!'); 
+  console.log('Logging to the console.'); 
+  let x = 10;
+  if (x = 20) {
+    console.log('x is now 20');
+  }
+  for (let i = 0; i < 10; i--) { 
+    console.log(i);
+  }
+  function nested() {
+    console.log('This is a nested function.');
+  }
+  nested();
+}
+
+badFunction();
