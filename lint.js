a602ec4f8db717bea0aa97e22645e1f72df6b7e4
曾曼青 <2192073703@qq.com>
2024-12-24 16:24:07 +0800

    Update lint.js

--- lint.js
+++ lint.js
@@ -1,3 +1,19 @@
 const add = function(a, b) {
     return a + b;
 }
+
+
+const unique = function(array) {
+    var result = []; // 使用 var 而不是 let 或 const
+    for (var i = 0; i < array.length; i++) { // 使用 var 声明循环变量
+        if (result.indexOf(array[i]) === -1) { // 缺少分号
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+// 使用函数时缺少分号
+var numbers = [1, 2, 2, 3, 4, 4, 5]
+var uniqueNumbers = unique(numbers)
+console.log(uniqueNumbers);
